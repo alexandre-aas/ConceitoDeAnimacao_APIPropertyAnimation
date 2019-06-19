63695f2c3427cca2e1719ebf9af0bdfe8d7fe447
Alexandre <aas@unochapeco.edu.br>
2019-06-19 15:19:51 -0300

    Criação do projeto. Inclusão dos primeiros fontes

---
+++ app/src/main/java/com/s/d/a/a/conceitodeanimacao_apipropertyanimation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.s.d.a.a.conceitodeanimacao_apipropertyanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
